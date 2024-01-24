255a056b9e460cb645ae0a5f818d74a9df9f082f
Gleiton Brito Bernardes <154294770+Gleitonbb@users.noreply.github.com>
2024-01-23 21:50:08 -0300

    treinamento em andamento

---
+++ treinamento.js
@@ -0,0 +1,6 @@
+const caixa1 = document.querySelector('#caixa1')
+
+// console.log(caixa1.firstElementChild)
+// console.log(caixa1.lastElementChild)
+// console.log(caixa1.children)
+console.log(document.getRootNode())
